973fb7868277ebfe75a8dfe5f0d0e5f7fecd2ded
Reshilobeth <92940855+Reshilobeth@users.noreply.github.com>
2022-06-09 08:42:37 +0530

    Add files via upload

---
+++ home/app.js
@@ -0,0 +1,62 @@
+// gsap.registerPlugin(ScrollTrigger)
+
+
+// const t1 = gsap.timeline()
+
+// t1.from('.nav-container', {duration:1.5, opacity:'0px', y:'-100px'})
+//    .from('.link',{duration:1,y:'-100px',stagger:.2, delay:-2,ease:'ease-in-out'})
+//    .to('.title',{stagger:.2,clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',y:'-30px'}, '=-1.5')
+//    .to('.here-img-container',{duration:1, clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)'},'=-1.5')
+//    .from('.main-para, .learn-more',{opacity:0 ,ease:'ease-in-out'})
+
+//    ScrollTrigger.create({
+//       trigger:'.projects',
+//       // markers:true,
+//       start:'top 90%',
+//       end:'bottom 60%',
+//       pin:'.pro-title',
+//       pinSpacing:false,
+//    })
+
+//    gsap.from('.rc-1',{
+//       opacity:0,
+//       y:30,
+//       scrollTrigger:{
+//          scrub:true,
+//          trigger:'.rc-1',
+//          markers:true,
+//          start:'top 75%',
+//          end:'bottom 50%',
+//       },     
+//    })
+//    gsap.from('.rc-2',{
+//       opacity:0,
+//       y:30,
+//       scrollTrigger:{
+//          scrub:true,
+//          trigger:'.rc-2',
+//          markers:true,
+//          start:'top 75%',
+//          end:'bottom 50%',
+//       },     
+//    })
+//    gsap.from('.rc-3',{
+//       opacity:0,
+//       y:30,
+//       scrollTrigger:{
+//          scrub:true,
+//          trigger:'.rc-3',
+//          markers:true,
+//          start:'top 75%',
+//          end:'bottom 50%',
+//       },     
+//    })
+
+   
+
+   
+
+
+
+
+
